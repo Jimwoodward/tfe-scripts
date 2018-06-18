2ae248deb9b75ac39aeee95714b8090864d86cf0
Jim Woodward <jim.woodward@snagajob.com>
2018-06-18 12:01:13 -0400

    Change payload construction

--- add_team_members.py
+++ add_team_members.py
@@ -4,13 +4,17 @@ import sys
 import json
 
 team_id = str(sys.argv[1])
-payload = sys.argv[2]
+payload_path = sys.argv[2]
 
 print (team_id)
-print (payload)
+print (payload_path)
+
+with open(payload_path, 'r') as payload_file:
+    payload = json.load(payload_file)
+    print (payload)
 
 token = 'Bearer '+os.environ["TOKEN"]
-headers = {'Authorization': token}
+headers = {'Authorization': token, 'Content-Type': 'application/vnd.api+json'}
 
 response = requests.post('https://app.terraform.io/api/v2/teams/{}/relationships/users'.format(team_id), headers=headers, data=payload)
 
