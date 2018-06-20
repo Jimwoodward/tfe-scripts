5aee4c5a552477c5e2000fda314956519b921996
Jim Woodward <jim.woodward@snagajob.com>
2018-06-20 11:15:44 -0400

    Update list_workspace to a func

--- list_workspace_access.py
+++ list_workspace_access.py
@@ -3,26 +3,31 @@ import sys
 import requests
 
 workspace_id = str(sys.argv[1])
-token = 'Bearer '+os.environ['TOKEN']
-headers = {'Authorization': token}
 
-team_workspaces_parameters = {'filter[workspace][id]': '{}'.format(workspace_id)}
-team_workspaces_response = requests.get('https://app.terraform.io/api/v2/team-workspaces', headers=headers, params=team_workspaces_parameters)
+def list_workspace_access(workspace_id):
+    token = 'Bearer '+os.environ['TOKEN']
+    headers = {'Authorization': token}
 
-workspace_name_parameters = {'page[size]': '100', 'page[number]': '1'}
-workspace_name_response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=workspace_name_parameters)
+    team_workspaces_parameters = {'filter[workspace][id]': '{}'.format(workspace_id)}
+    team_workspaces_response = requests.get('https://app.terraform.io/api/v2/team-workspaces', headers=headers, params=team_workspaces_parameters)
 
-total_pages=workspace_name_response.json()['meta']['pagination']['total-pages']
+    workspace_name_parameters = {'page[size]': '100', 'page[number]': '1'}
+    workspace_name_response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=workspace_name_parameters)
 
-print (total_pages)
-for pages in range(1, total_pages+1):
-    parameters = {'page[size]': '100', 'page[number]': pages}
-    response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
+    total_pages=workspace_name_response.json()['meta']['pagination']['total-pages']
 
-    for workspace in response.json()["data"]:
-        if workspace['id'] == workspace_id:
-            print ('Workspace name: {}'.format(workspace["attributes"]["name"]))
+    print (total_pages)
+    for pages in range(1, total_pages+1):
+        parameters = {'page[size]': '100', 'page[number]': pages}
+        response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
 
-print ('Workspace id: {}'.format(workspace_id))
-for teams in team_workspaces_response.json()['data']:
-    print (teams['relationships']['team']['data']['id']+' ::::: '+teams['relationships']['team']['links']['related'].split('/')[4])
+        for workspace in response.json()["data"]:
+            if workspace['id'] == workspace_id:
+                print ('Workspace name: {}'.format(workspace["attributes"]["name"]))
+
+    print ('Workspace id: {}'.format(workspace_id))
+    for teams in team_workspaces_response.json()['data']:
+        print (teams['relationships']['team']['data']['id']+' ::::: '+teams['relationships']['team']['links']['related'].split('/')[4])
+
+
+list_workspace_access(workspace_id)
