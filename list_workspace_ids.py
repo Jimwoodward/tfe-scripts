b76bca6b04c6353f674a314de6e801e85ae75bc5
Jim Woodward <jim.woodward@snagajob.com>
2018-06-21 12:36:59 -0400

    Update listworkspace defn

--- list_workspace_ids.py
+++ list_workspace_ids.py
@@ -1,19 +1,8 @@
-import requests
 import os
+import sys
+from workspace import list_workspace_ids
 
+human_readable = sys.argv[1]
 token = 'Bearer '+os.environ['TOKEN']
-headers = {'Authorization': token}
 
-parameters = {'page[size]': '100', 'page[number]': '1'}
-response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
-total_pages=response.json()['meta']['pagination']['total-pages']
-
-file = open('workspace_ids.txt', 'w')
-
-for pages in range(1, total_pages+1):
-    parameters = {'page[size]': '100', 'page[number]':pages}
-    response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
-
-    for workspace in response.json()['data']:
-        file.write('Team name: {} ::::: '.format(workspace['attributes']['name']))
-        file.write('Workspace id: {}'.format(workspace['id']+'\n'))
+list_workspace_ids(token, human_readable)
--- workspace.py
+++ workspace.py
@@ -16,10 +16,39 @@ def list_workspace_access(token, workspace_id):
         parameters = {'page[size]': '100', 'page[number]': pages}
         response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
 
-        for workspace in response.json()["data"]:
+        for workspace in response.json()['data']:
             if workspace['id'] == workspace_id:
-                print ('Workspace name: {}'.format(workspace["attributes"]["name"]))
+                print ('Workspace name: {}'.format(workspace['attributes']['name']))
 
     print ('Workspace id: {}'.format(workspace_id))
     for teams in team_workspaces_response.json()['data']:
         print (teams['relationships']['team']['data']['id']+' ::::: '+teams['relationships']['team']['links']['related'].split('/')[4])
+
+
+def list_workspace_ids(token, human_readable):
+    headers = {'Authorization': token}
+
+    parameters = {'page[size]': '100', 'page[number]': '1'}
+    response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
+    total_pages=response.json()['meta']['pagination']['total-pages']
+
+    if human_readable == 'True':
+
+        file = open('workspace_ids.txt', 'w')
+
+        for pages in range(1, total_pages+1):
+            parameters = {'page[size]': '100', 'page[number]':pages}
+            response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
+
+            for workspace in response.json()['data']:
+                file.write('Team name: {} ::::: '.format(workspace['attributes']['name']))
+                file.write('Workspace id: {}'.format(workspace['id']+'\n'))
+    elif human_readable == 'False':
+        for pages in range(1, total_pages+1):
+            parameters = {'page[size]': '100', 'page[number]':pages}
+            response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=parameters)
+
+            for workspace in response.json()['data']:
+                return workspace["id"]
+    else:
+        print('"Human Readable" flag set incorrectly. The value should be "True" or "False". The value was: {}'.format(human_readable))
