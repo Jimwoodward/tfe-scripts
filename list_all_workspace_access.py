571bd48956d0290d1637163e2a379cbb46aa8ad5
Jim Woodward <jim.woodward@snagajob.com>
2018-06-22 14:45:01 -0400

    Finish list_all_workspaces function

--- list_all_workspace_access.py
+++ list_all_workspace_access.py
@@ -1,10 +1,9 @@
-import sys
 import os
 from workspace import list_workspace_access
 from workspace import list_workspace_ids
 
-human_readable = sys.argv[1]
 token = 'Bearer '+os.environ['TOKEN']
 
-for workspace_id in list_workspace_ids(token, False):
-    list_workspace_access(token, workspace_id)
+for workspace_id in list_workspace_ids(token, 'False'):
+    for results in list_workspace_access(token, workspace_id, 'True'):
+        print(results)
