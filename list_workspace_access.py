6e6164f72624f87d1bc3574be05ea3b9549b2f4c
Jim Woodward <jim.woodward@snagajob.com>
2018-06-22 14:08:41 -0400

    Fix the list_workspace_access function

--- list_workspace_access.py
+++ list_workspace_access.py
@@ -3,7 +3,8 @@ import sys
 from workspace import list_workspace_access
 
 workspace_id = str(sys.argv[1])
+human_readable = str(sys.argv[2])
 token = 'Bearer '+os.environ['TOKEN']
 
-for workspace_access in list_workspace_access(token, workspace_id):
+for workspace_access in list_workspace_access(token, workspace_id, human_readable):
     print(workspace_access)
--- workspace.py
+++ workspace.py
@@ -21,41 +21,35 @@ def list_workspace_ids(token, human_readable):
             for workspace in response.json()['data']:
                 yield workspace["id"]
 
-def list_workspace_access(token, workspace_id):
+def list_workspace_access(token, workspace_id, human_readable):
     headers = {'Authorization': token}
     team_workspaces_parameters = {'filter[workspace][id]': '{}'.format(workspace_id)}
     team_workspaces_response = requests.get('https://app.terraform.io/api/v2/team-workspaces', headers=headers, params=team_workspaces_parameters)
     response_payload = team_workspaces_response.json()
 
-    for teams in response_payload['data']:
-        yield teams['relationships']['team']['data']['id']
-        #print(teams['relationships']['team']['data']['id']+' ::::: '+teams['relationships']['team']['links']['related'].split('/')[4])
-        #workspace_payload = requests.get('https://app.terraform.io/{}'.format(teams['relationships']['workspace']['links']['related']))
-        #print(workspace_payload.url)
-        #print(workspace_payload)
+    if human_readable == 'False':
+        yield workspace_id
+        for teams in response_payload['data']:
+            yield teams['relationships']['team']['data']['id']
 
-        workspace_name_parameters = {'page[size]': '100', 'page[number]': '1'}
-        workspace_name_response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=workspace_name_parameters)
+    elif human_readable == 'True':
+        grab_next = True
+        page = 1
 
-        total_pages=workspace_name_response.json()['meta']['pagination']['total-pages']
+        while grab_next:
+            workspace_name_parameters = {'page[size]': '100', 'page[number]': page}
+            workspace_name_response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=workspace_name_parameters)
 
-        # grab_next = True
-        # page = 1
-        #
-        # while grab_next:
-        #     workspace_name_parameters = {'page[size]': '100', 'page[number]': page}
-        #     workspace_name_response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=workspace_name_parameters)
-        #     response_payload = workspace_name_response.json()
-        #
-        #     for workspace in response_payload['data']:
-        #         if workspace['id'] == workspace_id:
-        #             print ('Workspace name: {}'.format(workspace['attributes']['name']))
-        #
-        #     print ('Workspace id: {}'.format(workspace_id))
-        #     for teams in team_workspaces_response.json()['data']:
-        #         print (teams['relationships']['team']['data']['id']+' ::::: '+teams['relationships']['team']['links']['related'].split('/')[4])
-        #
-        #     page = response_payload['meta']['pagination']['next-page']
-        #
-        #     if page is None:
-        #         grab_next = False
+            response_payload1 = workspace_name_response.json()
+
+            for workspace in response_payload1['data']:
+                if workspace['id'] == workspace_id:
+                    yield 'Workspace name: {}'.format(workspace['attributes']['name'])
+
+            page = response_payload1['meta']['pagination']['next-page']
+
+            if page is None:
+                grab_next = False
+
+        for teams in response_payload['data']:
+            yield teams['relationships']['team']['data']['id']+' ::::: '+teams['relationships']['team']['links']['related'].split('/')[4]
