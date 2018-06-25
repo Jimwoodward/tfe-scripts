1eb7ead07325c05859fce898ac1ba8d3b698ce09
Jim Woodward <jim.woodward@snagajob.com>
2018-06-25 11:01:39 -0400

    Update

--- list_all_workspace_access.py
+++ list_all_workspace_access.py
@@ -1,9 +1,14 @@
 import os
+import time
 from workspace import list_workspace_access
 from workspace import list_workspace_ids
 
 token = 'Bearer '+os.environ['TOKEN']
 
+begin = time.time()
 for workspace_id in list_workspace_ids(token, 'False'):
-    for results in list_workspace_access(token, workspace_id, 'True'):
+    for results in list_workspace_access(token, workspace_id, 'False'):
         print(results)
+
+end = time.time()
+print(end - begin)
