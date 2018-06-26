e1d2bb4d90438cb95a63bdd9bfd56e363e8c0302
Jim Woodward <jim.woodward@snagajob.com>
2018-06-26 14:24:10 -0400

    Update workspace.py with raw flags/functionality

--- list_workspace_access.py
+++ list_workspace_access.py
@@ -5,6 +5,7 @@ from workspace import list_workspace_access
 workspace_id = str(sys.argv[1])
 human_readable = str(sys.argv[2])
 token = 'Bearer '+os.environ['TOKEN']
+lists = []
 
-for workspace_access in list_workspace_access(token, workspace_id, human_readable):
+for workspace_access in list_workspace_access(token, workspace_id, human_readable, lists):
     print(workspace_access)
--- workspace.py
+++ workspace.py
@@ -17,6 +17,7 @@ def create_workspace_ids_list(token):
             workspace_list.append(workspace)
 
         page = response_payload['meta']['pagination']['next-page']
+
         if page is None:
             grab_next = False
 
@@ -26,16 +27,23 @@ def create_workspace_ids_list(token):
 
 def list_workspace_ids(token, human_readable, workspaces_list):
     begin = time.time()
+
     if human_readable == 'True':
         for workspace in workspaces_list:
             yield 'Workspace name: {} ::::: Workspace id: {}'.format(workspace['attributes']['name'], workspace['id'])
+
     elif human_readable == 'False':
         for workspace in workspaces_list:
             yield workspace['id']
+
+    elif human_readable == "Raw":
+        for workspace in workspaces_list:
+            yield workspace
+
     end = time.time()
     print('list_workspace_ids runtime: {}'.format(end-begin))
 
-def list_workspace_access(token, workspace_id, human_readable):
+def list_workspace_access(token, workspace_id, human_readable, workspaces_list):
     headers = {'Authorization': token}
     team_workspaces_parameters = {'filter[workspace][id]': '{}'.format(workspace_id)}
     team_workspaces_response = requests.get('https://app.terraform.io/api/v2/team-workspaces', headers=headers, params=team_workspaces_parameters)
@@ -50,20 +58,13 @@ def list_workspace_access(token, workspace_id, human_readable):
         grab_next = True
         page = 1
 
-        while grab_next:
-            workspace_name_parameters = {'page[size]': '100', 'page[number]': page}
-            workspace_name_response = requests.get('https://app.terraform.io/api/v2/organizations/snag/workspaces', headers=headers, params=workspace_name_parameters)
-
-            response_payload1 = workspace_name_response.json()
-
-            for workspace in response_payload1['data']:
-                if workspace['id'] == workspace_id:
-                    yield 'Workspace name: {}'.format(workspace['attributes']['name'])
-
-            page = response_payload1['meta']['pagination']['next-page']
-
-            if page is None:
-                grab_next = False
+        for workspace in workspaces_list:
+            if workspace['id'] == workspace_id:
+                yield '{} ::::: Workspace name'.format(workspace['attributes']['name'])
 
         for teams in response_payload['data']:
-            yield teams['relationships']['team']['data']['id']+' ::::: '+teams['relationships']['team']['links']['related'].split('/')[4]
+            yield teams['relationships']['team']['links']['related'].split('/')[4]+' ::::: '+teams['relationships']['team']['data']['id']
+
+    elif human_readable == 'Raw':
+        yield workspace_id
+        yield response_payload
