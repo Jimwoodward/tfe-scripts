4af3c65123a80ae7b24fc063b8ba0a7ea6635025
Jim Woodward <jim.woodward@snagajob.com>
2018-06-27 22:56:09 -0400

    Update list_teams_ids.py to use new functions in teams.py

---
+++ teams_api/list_team_ids.py
@@ -0,0 +1,6 @@
+from teams import call_teams
+
+response_payload = call_teams()
+
+for team in response_payload['data']:
+    print("Team name: {}".format(team["attributes"]["name"])+" ::::: "+"Team id: {}".format(team["id"]))
---
+++ teams_api/teams.py
@@ -0,0 +1,22 @@
+import requests, os
+
+def call_teams():
+    token = 'Bearer '+os.environ['TOKEN']
+    headers = {'Authorization': token}
+    teams_response = requests.get('https://app.terraform.io/api/v2/organizations/snag/teams', headers=headers)
+    response_payload = teams_response.json()
+    return response_payload
+
+# def list_team_names():
+#     response_payload = call_teams()
+
+#     for team in response_payload['data']:
+#         yield team['attributes']['name']
+
+# def list_team_members(team_id):
+#     response_payload = call_teams()
+
+#     for team in response_payload['data']
+
+# def list_team_ids():
+
