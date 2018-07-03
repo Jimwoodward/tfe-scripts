37785f3cfb7a755be253525219b8b3eec7857d79
Jim Woodward <jim.woodward@snagajob.com>
2018-07-03 15:30:25 -0400

    Create new team workspaces call with method POST

--- team_workspaces_api/team_workspaces.py
+++ team_workspaces_api/team_workspaces.py
@@ -2,13 +2,20 @@ import requests
 import time
 import os
 
-def call_team_workspaces(workspace_id):
+def call_team_workspaces_get(workspace_id):
     token = 'Bearer '+os.environ['TOKEN']
     headers = {'Authorization': token}
     team_workspaces_parameters = {'filter[workspace][id]': '{}'.format(workspace_id)}
     team_workspaces_response = requests.get('https://app.terraform.io/api/v2/team-workspaces', headers=headers, params=team_workspaces_parameters)
     response_payload = team_workspaces_response.json()
     return response_payload
+
+def call_team_workspaces_post(access_level, workspace_id, team_id):
+    token = 'Bearer '+os.environ['TOKEN']
+    headers = {'Authorization': token, 'Content-Type': 'application/vnd.api+json'}
+    payload = {'data':{'attributes':{'access':access_level},'relationships':{'workspace':{'data':{'type':'workspaces','id':workspace_id}},'team':{'data':{'type':'teams','id':team_id}}},'type':'team-workspaces'}}
+    team_workspaces_response = requests.post('https://app.terraform.io/api/v2/team-workspaces', headers=headers, json=payload)
+    print(team_workspaces_response.raise_for_status)
 
 def list_workspace_access_verbose(workspaces_list, workspace_id):
     response_payload = call_team_workspaces(workspace_id)
